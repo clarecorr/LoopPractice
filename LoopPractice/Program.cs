759d7779e194896ae1784877ac8fe4ea048409ed
clarecorr <clare.e.corrigan@gmail.com>
2017-07-05 15:15:31 -0400

    reviewed concepts

--- LoopPractice/Program.cs
+++ LoopPractice/Program.cs
@@ -10,6 +10,19 @@ namespace LoopPractice
     {
         static void Main(string[] args)
         {
+            //when you would use a loop
+            //why you would use a loop
+            //nested loops
+            //infinite loops
+            //keywords break and continue
+
+            //For Loop
+            //can be used for both counting and iterating through an array (or other collection type)
+            //Looks like this:
+            //for(initializer); condition; updater)
+            //{
+            //      do something
+            //}
         }
     }
 }
